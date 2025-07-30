ee095c0b2fdfa1e59020630a6748b2a6dfbc2d66
anghelonyo <karlbasconcillo5@gmail.com>
2025-07-30 02:13:53 +0000

    ACTIVITY 3

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("BASCONCILLO, KARL ANGELO P.");
+Console.WriteLine("20221124523");
+Console.WriteLine("'Wag na muna natin pag-usapan");
